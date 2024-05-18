66ad8274c609c1837485e43a8a620ce9525e93d1
Taras Mamchits <tmamchitspc@gmail.com>
2024-05-18 08:21:27 -0700

    added function to list users

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -21,8 +21,15 @@ function UsersList() {
         return <div>Error fetching data...</div>
     }
 
+    const renderedUsers = data.map((user) => {
+        return <div key={user.id} className="mb-2 border rounded">
+            <div className="flex p-2 justify-between items-center cursor-pointer" />
+            {user.name}
+        </div>
+    })
+
     return <div>
-        {data.length}
+        {renderedUsers}
     </div>
 }
 
