3c9afe437275888db3e26640714307b6563cdea6
Taras Mamchits <tmamchitspc@gmail.com>
2024-05-18 12:07:25 -0700

    add function to delete user

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -3,7 +3,8 @@ import { useSelector } from "react-redux"
 import { fetchUsers, addUser } from '../store'
 import Skeleton from "./Skeleton"
 import Button from './Button'
-import { useThunk } from "../hooks/useThunk"
+import { useThunk } from "../hooks/use-thunk"
+import UsersListItem from "./UsersListItem"
 
 function UsersList() {
     const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers)
@@ -28,10 +29,7 @@ function UsersList() {
         content = <div>Error fetching data...</div>
     } else {
         content = data.map((user) => {
-            return <div key={user.id} className="mb-2 border rounded">
-                <div className="flex p-2 justify-between items-center cursor-pointer" />
-                {user.name}
-            </div>
+            return <UsersListItem key={user.id} user={user} />
         })
     }
 
